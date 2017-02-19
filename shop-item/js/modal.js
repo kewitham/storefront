b0e38dae1c4d2afbc85efe1c853a96ef90fb008c
katie <kewitham@gmail.com>
2017-02-19 14:35:11 -0800

    added dropdown and button functionality

--- shop-item/js/modal.js
+++ shop-item/js/modal.js
@@ -90,4 +90,34 @@ $(document).ready(function() {
    $('#imagemodal22').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
 });
 
+  $('.btn-size').click(function () {
+            $('.btn-size.active').removeClass("active");
+            $(this).addClass("active");
+        });
+  $('.btn-shade').click(function () {
+            $('.btn-shade.active').removeClass("active");
+            $(this).addClass("active");
+        });
+
+
+
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
